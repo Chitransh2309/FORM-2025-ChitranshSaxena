9df2b673defddef1e7249a5ab564a69e0facbc95
fabjenny13 <65107441+fabjenny13@users.noreply.github.com>
2025-06-19 11:51:14 +0530

    added a blank left panel (light theme)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 import Image from "next/image";
 
+import LeftPanel from "./components/left-panel";
+import { Homemade_Apple } from "next/font/google";
+
 export default function Home() {
   return (
-    <div className="flex items-center justify-center w-full h-screen">
+    <div style={{ display: "flex", height: "100vh" }}>
+      <LeftPanel />
       <div className="flex items-center justify-center w-full h-screen">
-        Hello. This is the form project cycle 2025.
+        <div className="flex items-center justify-center w-full h-screen">
+          Hello. This is the form project cycle 2025.
+        </div>
       </div>
     </div>
   );
