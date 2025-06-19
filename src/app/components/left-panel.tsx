7b7d145d6f4f65aa1c81b804c679eeeda66af27e
fabjenny13 <65107441+fabjenny13@users.noreply.github.com>
2025-06-19 14:14:18 +0530

    left panel is divided into sections and endings, and has a username footer.

--- src/app/components/left-panel.tsx
+++ src/app/components/left-panel.tsx
@@ -1,7 +1,20 @@
 import "../form-builder.css";
 
 function LeftPanel() {
-  return <nav className="left-panel"></nav>;
+  return (
+    <nav className="left-panel">
+      <div className="section-content">
+        <h4>SECTIONS</h4>
+        <div className="section-divider"></div>
+      </div>
+
+      <div className="end-page">
+        <h4>ENDINGS</h4>
+        <div className="section-divider"></div>
+      </div>
+      <div className="username-footer">UserName</div>
+    </nav>
+  );
 }
 
 export default LeftPanel;
