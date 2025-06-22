9f0baadcb4e7201a3d254f998265f101b467f942
TarunRam-git <tarunram23@gmail.com>
2025-06-22 23:51:17 +0530

    highlight box

---
+++ src/lib/mongodb.ts
@@ -0,0 +1,14 @@
+
+import { Db, MongoClient } from "mongodb";
+
+export async function connectToDB() {
+    const dbClient = new MongoClient(process.env.MONGODB_URI || "")
+    await dbClient.connect();
+    const db = dbClient.db("formdb");
+
+    return {dbClient, db};
+}
+
+export async function disconnectFromDB(dbClient: MongoClient) {
+    await dbClient.close();
+}
