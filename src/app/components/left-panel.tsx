80129536322ba68c6749b84eb383596c42d70a20
fabjenny13 <65107441+fabjenny13@users.noreply.github.com>
2025-06-23 19:17:13 +0530

    removed form-builder.css and implemented tailwindcss

--- src/app/components/left-panel.tsx
+++ src/app/components/left-panel.tsx
@@ -1,18 +1,18 @@
-import "../form-builder.css";
-
 function LeftPanel() {
   return (
-    <nav className="left-panel">
-      <div className="section-content">
+    <nav className="flex flex-col bg-[#fefefe] text-black w-1/5 h-screen p-4 box-border font-[Outfit]">
+      <div className="text-sm">
         <h4>SECTIONS</h4>
-        <div className="section-divider"></div>
+        <div className="border-t-2 border-black my-2"></div>
+        <div className="h-80 overflow-y-auto scrollbar-hidden"></div>
       </div>
 
-      <div className="endings-content">
+      <div className="text-sm my-5">
         <h4>ENDINGS</h4>
-        <div className="section-divider"></div>
+        <div className="border-t-2 border-black my-2"></div>
+        <div className="h-20 overflow-y-auto scrollbar-hidden"></div>
       </div>
-      <div className="username-footer">UserName</div>
+      <footer className="fixed-bottom text-xs">UserName</footer>
     </nav>
   );
 }
