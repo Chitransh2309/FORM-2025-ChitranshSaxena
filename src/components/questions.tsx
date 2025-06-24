1bc48f1941d512741a3f35dc47f4fc44374a070b
fabjenny13 <65107441+fabjenny13@users.noreply.github.com>
2025-06-24 20:53:28 +0530

    Revert "fixed goofy layout"
    This reverts commit 60693ee667d24e288c4588b8ed051d434f79cf88.

--- src/components/questions.tsx
+++ src/components/questions.tsx
@@ -62,7 +62,7 @@ export default function Question({
           placeholder="Ques Label *"
           className="focus:outline-none font-bold text-xl text-black"
           value={data.label}
-          onChange={(e) => onUpdate(id, { label: e.target.value })}
+          onChange={e => onUpdate(id, { label: e.target.value })}
         />
 
         <div className="flex items-center">
@@ -76,7 +76,7 @@ export default function Question({
               id={toggleId}
               className="sr-only peer"
               checked={data.required}
-              onChange={(e) => onUpdate(id, { required: e.target.checked })}
+              onChange={e => onUpdate(id, { required: e.target.checked })}
             />
             <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border after:border-gray-300 after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
           </label>
@@ -96,7 +96,7 @@ export default function Question({
           placeholder="Write your question here *"
           className="resize-none focus:outline-none w-[75%] min-h-[10px] overflow-hidden p-0"
           value={data.content}
-          onChange={(e) => onUpdate(id, { content: e.target.value })}
+          onChange={e => onUpdate(id, { content: e.target.value })}
         />
       </div>
 
