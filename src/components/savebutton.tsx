5ec83b96b7f13d0e7a3cf18bb1f41c7d02502d10
fabjenny13 <65107441+fabjenny13@users.noreply.github.com>
2025-06-24 19:16:40 +0530

    removed one scrollbar

--- src/components/savebutton.tsx
+++ src/components/savebutton.tsx
@@ -1,11 +1,11 @@
 // app/components/savebutton.tsx
-import React from 'react';
+import React from "react";
 
 export default function Save({ onClick }: { onClick: () => void }) {
   return (
     <button
       onClick={onClick}
-      className="bg-[#61A986] text-white cursor-pointer hover:bg-green-300 h-10 w-20 rounded-lg"
+      className="bg-[#61A986] text-white cursor-pointer hover:bg-[#628a76] h-10 w-20 rounded-lg"
     >
       Save
     </button>
