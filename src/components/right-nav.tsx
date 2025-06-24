2d6af91ec5c777d14d76eeabb4b094e03034fb80
fabjenny13 <65107441+fabjenny13@users.noreply.github.com>
2025-06-24 18:51:32 +0530

    changed dimensions of nav bars

--- src/components/right-nav.tsx
+++ src/components/right-nav.tsx
@@ -1,15 +1,15 @@
-'use client';
+"use client";
 
 import React from "react";
 import Dropdown from "./right-nav-dropdown";
 
-export default function RightNav(){
-    // const [type, isType] = useState("Text");
+export default function RightNav() {
+  // const [type, isType] = useState("Text");
 
-    return (
-        <div className="m-10">
-            <p className="mb-5 font-semibold">Description</p>
-            <Dropdown />
-        </div>
-    );
+  return (
+    <div className="p-4 mt-8">
+      <p className="mb-5 font-semibold">Description</p>
+      <Dropdown />
+    </div>
+  );
 }
