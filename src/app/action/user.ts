8de73e498f1343f3751b33eca823f6a7a124cb9f
Ishhan Kheria <ishhankheria06@gmail.com>
2025-06-25 19:12:51 +0530

    Adding mongodb.ts

--- src/app/action/user.ts
+++ src/app/action/user.ts
@@ -1,26 +1,23 @@
-import { connectToDB, disconnectFromDB } from "@/lib/mongoDB";
+import { connectToDB, disconnectFromDB } from "@/lib/mongodb";
 import { auth } from "../../../auth";
 import { nanoid } from "nanoid";
 
-
 export async function insertUser() {
-    const { db, dbClient } = await connectToDB();
-    const session= await auth();
-    if(session&&session.user)
-    {
-        const {name,email,id,image}=session.user;
-        const existingUser = await db.collection("user").findOne({email});
-        if(!existingUser)
-        {
-            await db.collection("user").insertOne({
-            user_ID: nanoid(),
-            email: email,
-            googleID: id,
-            name: name,
-            image: image,
-            forms: [],
-            });
-        }
+  const { db, dbClient } = await connectToDB();
+  const session = await auth();
+  if (session && session.user) {
+    const { name, email, id, image } = session.user;
+    const existingUser = await db.collection("user").findOne({ email });
+    if (!existingUser) {
+      await db.collection("user").insertOne({
+        user_ID: nanoid(),
+        email: email,
+        googleID: id,
+        name: name,
+        image: image,
+        forms: [],
+      });
     }
-    await disconnectFromDB(dbClient);
+  }
+  await disconnectFromDB(dbClient);
 }
