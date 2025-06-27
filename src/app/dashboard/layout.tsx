f2adf002d6d995e641a7ef8090e7ea09e0b16e54
fabjenny13 <65107441+fabjenny13@users.noreply.github.com>
2025-06-27 09:14:31 +0530

    gradient looks less disgusting

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="bg-gradient-to-r from-green-100 via-[#f1f8f5] to-green-100  min-h-screen w-full">
+    <div className="bg-gradient-to-r from-green-100/50 via-[#f1f8f5] to-green-100/50  min-h-screen w-full">
       {children}
     </div>
   );
