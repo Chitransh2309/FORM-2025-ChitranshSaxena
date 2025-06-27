bad15aa41431871bcdda372aa2e9f2f6c3257b16
fabjenny13 <65107441+fabjenny13@users.noreply.github.com>
2025-06-28 00:03:47 +0530

    mitigated hydration error

--- src/components/NewUserPage/SearchBar.tsx
+++ src/components/NewUserPage/SearchBar.tsx
@@ -1,7 +1,11 @@
+"use client"; // if using Next.js
+
 import { FaSearch } from "react-icons/fa";
 import { RiExpandUpDownLine } from "react-icons/ri";
+import { useState, useEffect } from "react";
 
 const Searchbar = () => {
+<<<<<<< HEAD:src/components/NewUserPage/SearchBar.tsx
     return (
         <div className="flex items-center gap-3 text-gray-600">
             <FaSearch size={16}/>
@@ -15,3 +19,27 @@ const Searchbar = () => {
     )
 }
 export default Searchbar 
+=======
+  const [hydrated, setHydrated] = useState(false);
+
+  useEffect(() => {
+    setHydrated(true);
+  }, []);
+
+  if (!hydrated) return null;
+
+  return (
+    <div className="flex items-center gap-3 text-gray-600">
+      <FaSearch size={16} />
+      <input
+        placeholder="Search"
+        type="text"
+        className="flex-1 bg-transparent outline-none text-black dark:text-white placeholder-gray-500"
+      />
+      <RiExpandUpDownLine size={16} />
+    </div>
+  );
+};
+
+export default Searchbar;
+>>>>>>> 902ba78 (mitigated hydration error):src/components/New_user_page/Searchbar.tsx
