a7224ab924a4aa8cbb0df4b07462922e4110efd1
Snigdha priya <snigdhapriya2004@gmail.com>
2025-07-03 13:11:00 +0530

    refactor: integrate FAQs component into Navbar support icon for mobile vew

--- src/components/NewUserPage/NavBar.tsx
+++ src/components/NewUserPage/NavBar.tsx
@@ -1,12 +1,13 @@
 "use client";
-
+import { useState } from "react";
 import Image from "next/image";
 import { FaRegCircleUser } from "react-icons/fa6";
 import { HiOutlineQuestionMarkCircle } from "react-icons/hi2";
 import ToggleSwitch from "../LandingPage/ToggleSwitch";
-
+import FAQs from "./FAQs";
 
 export default function Navbar() {
+  const [showFaq, setShowFaq] = useState(false);
   return (
     /* mobile‑only green bar */
     <div className="xl:hidden w-full flex items-center justify-between px-4 py-3 bg-[#56A37D] dark:bg-[#2B2A2A]">
@@ -26,9 +27,15 @@ export default function Navbar() {
       {/* right: toggle + icons */}
       <div className="flex items-center gap-4">
         <ToggleSwitch />
-        <HiOutlineQuestionMarkCircle size={26} className="text-black hover:text-gray-700 dark:text-white dark:hover:text-gray-300" />
+        <button onClick={() => setShowFaq(true)}>
+           <HiOutlineQuestionMarkCircle
+             size={26}
+             className="text-black hover:text-gray-700 dark:text-white dark:hover:text-gray-300"
+           />
+         </button>
         <FaRegCircleUser         size={20} className="text-black hover:text-gray-700 dark:text-white dark:hover:text-gray-300" />
       </div>
+      {showFaq && <FAQs showFaq={showFaq} setShowFaq={setShowFaq} />}
     </div>
   );
 }
