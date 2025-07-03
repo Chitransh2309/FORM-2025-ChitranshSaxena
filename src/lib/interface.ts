9d5aa0b01e00314386362f9c655bf31c5e5d1871
adhyan-jain <adhyanjain2006@gmail.com>
2025-07-03 19:21:12 +0530

    fixed preview and one bug in question component

--- src/lib/interface.ts
+++ src/lib/interface.ts
@@ -150,7 +150,7 @@ export interface FieldType {
 
 export const fieldtypes: FieldType[] = [
   {
-    name: "TEXT",
+    name: "text",
     type: "string",
     params: [{ name: "placeholder", type: "string" }],
     validations: [
@@ -171,7 +171,7 @@ export const fieldtypes: FieldType[] = [
     ],
   },
   {
-    name: "MCQ",
+    name: "mcq",
     type: "checkBox",
     params: [
       { name: "options", type: "array[string]" },
@@ -182,27 +182,27 @@ export const fieldtypes: FieldType[] = [
     validations: [],
   },
   {
-    name: "DROPDOWN",
+    name: "dropdown",
     type: "string",
     params: [{ name: "options", type: "array[string]" }],
     validations: [],
   },
   {
-    name: "DATE",
+    name: "date",
     type: "date",
     params: [{ name: "includeTime", type: "boolean" }],
     validations: [
       {
         name: "dateRange",
         params: [
-          { name: "minDate", type: "string" },
-          { name: "maxDate", type: "string" },
+          { name: "minDate", type: "date" },
+          { name: "maxDate", type: "date" },
         ],
       },
     ],
   },
   {
-    name: "LINEARSCALE",
+    name: "linear_scale",
     type: "number",
     params: [
       { name: "min", type: "number" },
@@ -213,7 +213,7 @@ export const fieldtypes: FieldType[] = [
     validations: [],
   },
   {
-    name: "FILE_UPLOAD",
+    name: "file_upload",
     type: "file",
     params: [
       { name: "allowedFileTypes", type: "array[string]" },
@@ -223,7 +223,7 @@ export const fieldtypes: FieldType[] = [
     validations: [],
   },
   {
-    name: "EMAIL",
+    name: "email",
     type: "string",
     params: [],
     validations: [
@@ -239,7 +239,7 @@ export const fieldtypes: FieldType[] = [
     ],
   },
   {
-    name: "URL",
+    name: "url",
     type: "string",
     params: [],
     validations: [
