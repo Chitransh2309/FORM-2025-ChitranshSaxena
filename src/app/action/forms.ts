58b538d44c1a80cbf18732fcae97cbdfbd0448de
TarunRam-git <tarunram23@gmail.com>
2025-07-13 14:27:38 +0530

    make the form timings work

--- src/app/action/forms.ts
+++ src/app/action/forms.ts
@@ -110,6 +110,8 @@ export async function getFormsForUser() {
       createdAt: form.createdAt?.toString() || null,
       publishedAt: form.publishedAt?.toString() || null,
       isActive: form.isActive || false,
+      settings: form.settings || {},
+
       isStarred: form.isStarred || false,
     }));
   } catch (error) {
