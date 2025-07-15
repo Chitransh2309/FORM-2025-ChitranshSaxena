23d73e16a57dbb0b57f0d06f361115db6b5682eb
fabjenny13 <65107441+fabjenny13@users.noreply.github.com>
2025-07-15 23:18:44 +0530

    formheader loader

--- src/components/FormPage/FormHeader.tsx
+++ src/components/FormPage/FormHeader.tsx
@@ -9,6 +9,7 @@ import FormSettings from "./FormSettings";
 import { Form, FormSettings as FormSettingsType } from "@/lib/interface";
 import { Settings, ChevronLeft } from "lucide-react";
 import getFormObject from "@/app/action/getFormObject";
+import Loader from "../Loader";
 
 export default function FormHeader({
   children,
@@ -23,6 +24,7 @@ export default function FormHeader({
   const [formLink, setFormLink] = useState<string | null>(null);
   const [showSettings, setShowSettings] = useState(false);
   const [formName, setFormName] = useState("Draft");
+  const [loading, setLoading] = useState(false);
 
   const isResponsePage = pathname.endsWith("/response");
 
@@ -51,6 +53,7 @@ export default function FormHeader({
   };
 
   const handleWorkspace = () => {
+    setLoading(true);
     router.push("/dashboard");
   };
 
@@ -62,68 +65,75 @@ export default function FormHeader({
     form.settings
   );
   return (
-    <div className="font-[Outfit]">
-      {!isResponsePage && (
-        <nav className="fixed top-0 left-0 w-full h-[75px] overflow-hidden min-h-0 bg-neutral-600 text-white px-4 flex justify-between items-center text-lg z-50">
-          <div className="flex-1 hidden md:block">
-            <button onClick={handleWorkspace} className="flex flex-row">
-              <ChevronLeft className="mt-1" /> Back to Workspace
-            </button>
-          </div>
-
-          <div className="flex flex-row items-center space-x-1">
-            <div className="md:hidden">
-              <button onClick={handleWorkspace}>
-                <ChevronLeft className="mt-1" />
+    <>
+      {loading && (
+        <div className="z-60">
+          <Loader />
+        </div>
+      )}
+      <div className="font-[Outfit]">
+        {!isResponsePage && (
+          <nav className="fixed top-0 left-0 w-full h-[75px] overflow-hidden min-h-0 bg-neutral-600 text-white px-4 flex justify-between items-center text-lg z-50">
+            <div className="flex-1 hidden md:block">
+              <button onClick={handleWorkspace} className="flex flex-row">
+                <ChevronLeft className="mt-1" /> Back to Workspace
               </button>
             </div>
-            <div className="flex-1 text-center">
-              <h2 className="font-semibold">{formName}</h2>
+
+            <div className="flex flex-row items-center space-x-1">
+              <div className="md:hidden">
+                <button onClick={handleWorkspace}>
+                  <ChevronLeft className="mt-1" />
+                </button>
+              </div>
+              <div className="flex-1 text-center">
+                <h2 className="font-semibold">{formName}</h2>
+              </div>
             </div>
+
+            <div className="flex-1 flex justify-end items-center space-x-0 md:space-x-4">
+              <ToggleSwitch />
+              <button onClick={() => setShowSettings(true)} className="px-3">
+                <Settings />
+              </button>
+              {showSettings && (
+                <FormSettings
+                  formId={form.form_ID}
+                  formSettings={formSettings}
+                  setFormSettings={setFormSettings}
+                  onClose={() => setShowSettings(false)}
+                />
+              )}
+
+              <button
+                onClick={handlePublish}
+                disabled={isPending}
+                className={`h-6 w-20 text-xs lg:text-sm lg:h-7 lg:w-25 rounded-md text-white font-semibold transition duration-200 ${
+                  isPending
+                    ? "bg-[#61A986] cursor-not-allowed"
+                    : "bg-[#61A986] hover:bg-[#43755d]"
+                }`}
+              >
+                {isPending ? "Publishing..." : "Publish"}
+              </button>
+            </div>
+          </nav>
+        )}
+
+        <div
+          className={`h-screen overflow-hidden ${
+            !isResponsePage ? "pt-[75px]" : ""
+          } bg-neutral-600`}
+        >
+          <div className="bg-neutral-100 text-black w-screen h-screen flex">
+            <div className="w-full h-full overflow-auto">{children}</div>
           </div>
-
-          <div className="flex-1 flex justify-end items-center space-x-0 md:space-x-4">
-            <ToggleSwitch />
-            <button onClick={() => setShowSettings(true)} className="px-3">
-              <Settings />
-            </button>
-            {showSettings && (
-              <FormSettings
-                formId={form.form_ID}
-                formSettings={formSettings}
-                setFormSettings={setFormSettings}
-                onClose={() => setShowSettings(false)}
-              />
-            )}
-
-            <button
-              onClick={handlePublish}
-              disabled={isPending}
-              className={`h-6 w-20 text-xs lg:text-sm lg:h-7 lg:w-25 rounded-md text-white font-semibold transition duration-200 ${
-                isPending
-                  ? "bg-[#61A986] cursor-not-allowed"
-                  : "bg-[#61A986] hover:bg-[#43755d]"
-              }`}
-            >
-              {isPending ? "Publishing..." : "Publish"}
-            </button>
-          </div>
-        </nav>
-      )}
-
-      <div
-        className={`h-screen overflow-hidden ${
-          !isResponsePage ? "pt-[75px]" : ""
-        } bg-neutral-600`}
-      >
-        <div className="bg-neutral-100 text-black w-screen h-screen flex">
-          <div className="w-full h-full overflow-auto">{children}</div>
         </div>
-      </div>
 
-      {formLink && (
-        <FormPublishModal formLink={formLink} onClose={handleCloseModal} />
-      )}
-    </div>
+        {formLink && (
+          <FormPublishModal formLink={formLink} onClose={handleCloseModal} />
+        )}
+      </div>
+    </>
   );
 }
