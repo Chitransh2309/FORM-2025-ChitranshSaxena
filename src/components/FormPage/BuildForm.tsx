529d28750a7518ecf4b6ce17f1516c530475fea3
fabjenny13 <65107441+fabjenny13@users.noreply.github.com>
2025-07-20 23:57:57 +0530

    added back right nav for mobile

--- src/components/FormPage/BuildForm.tsx
+++ src/components/FormPage/BuildForm.tsx
@@ -435,12 +435,12 @@ export default function BuildPage({
                 Close
               </button>
             </div>
-            {/* <div className="p-4">
+            <div className="p-4">
               <RightNav
                 selectedQuestion={selectedQuestion}
                 onUpdate={updateQuestion}
               />
-            </div> */}
+            </div>
           </div>
         )}
         {showFAQ && <FAQs showFaq={showFAQ} setShowFaq={setShowFAQ} />}
